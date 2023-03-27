bf4baf71c684b8abd40b597ef3efae11ea970515
Francisco Sampaio <franciscohynt@outlook.com>
2023-03-26 23:37:53 -0300

    feat: initial commit

---
+++ src/main/java/en/com/avenuecode/filesapi/FilesApiApplication.java
@@ -0,0 +1,13 @@
+package en.com.avenuecode.filesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesApiApplication.class, args);
+	}
+
+}
